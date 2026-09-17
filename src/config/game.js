4446069ed31b48a25ed38a9681fa89e0968392c6
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-18 00:19:13 +0200

    Bump version to v0.13.0

--- src/config/game.js
+++ src/config/game.js
@@ -1,6 +1,6 @@
 export const GAME_CONFIG = {
   title: 'RAIDFORGE',
-  version: '0.12.0',
+  version: '0.13.0',
   world: {
     // Fallback arena size. Individual bosses should define their own arena.
     width: 1800,
